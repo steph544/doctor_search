f5578f10325d87a67a815dcf6ef4c29b786dafb8
Stephanie Cheney <steph@scheneyphotography.com>
2020-05-04 17:56:24 -0500

    Seeded data with methods write up

--- lib/app.rb
+++ lib/app.rb
@@ -13,3 +13,20 @@ $prompt = TTY::Prompt.new
 #     userInfo
 # end
 
+1. Create User or Check if User exists
+    -If exists, return user name 
+    -load favs_list
+        -all them to add new favorite doctors 
+    -If not, then create new user 
+        -Begin searching for doctors
+
+2. Search 
+    -Search by location, or specialty 
+    -Search database based on what user chooses 
+    -return all the matches 
+
+3. After searching, user will choose one to update, delete, or add to list. 
+
+4. Would you like to search for more options or save your favs list? 
+
+5. If save, save the favs list and say "Your list has been saved. Thank you for searching" or else exit "Thanks for searching."
